2aab955a2f902cef403e3c763e3da8ff92d735e8
kkostikov <kkostikov@pushwoosh.com>
2020-04-12 20:01:28 +0300

    Added Eslint

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ install:
 start:
 	node bin/brain-games.js
 
+lint:
+	npx eslint .
+
 publish:
 	npm publish --dry-run
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import readlineSync from 'readline-sync';
 const requestUsername = () => readlineSync.question('May I have your name? ');
 const greeting = (username) => console.log(`Hello, ${username}`);
 
-export const main = () => {
+const main = () => {
   console.log('Welcome to the Brain Games!');
 
   const username = requestUsername();
   greeting(username);
 };
+
+export default main;
